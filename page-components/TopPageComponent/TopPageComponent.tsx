08aa563dec6ccd45c760ec35d97b869696f3ee76
Elizaveta <lizastepanova93@mail.ru>
2023-01-31 23:12:36 +0700

    creating__product

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,5 +1,5 @@
 import { useReducer } from "react";
-import { Advantages, HhData, Htag, P, Sort, Tag } from "../../components";
+import { Advantages, HhData, Htag, P, Product, Sort, Tag } from "../../components";
 import { SortEnum } from "../../components/Sort/Sort.props";
 import { TopLevelCategory } from "../../interfaces/page.interface";
 import { sortReducer } from "./sort.reducer";
@@ -21,7 +21,7 @@ export const TopPageComponent = ({page, products, firstCategory}: TopPageCpmpone
                 <Sort sort={sort} setSort={setSort}/>
             </div>
             <div>
-                {sortedProducts && sortedProducts.map(p => <div key={p._id}>{p.title}</div>)}
+                {sortedProducts && sortedProducts.map(p => <Product key={p._id} product={p}/>)}
             </div>
             <div className={styles.hhtitle}>
                 <Htag tag="h2">Вакансии - {page.category}</Htag>
